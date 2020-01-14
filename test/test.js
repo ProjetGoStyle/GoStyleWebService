f30dda42b1edefec6c250f31819dfe8c96f4bc9c
AlexLecq <alex.lecq6@gmail.com>
2020-01-14 15:55:45 +0100

    change folder name to uppercase

--- test/test.js
+++ test/test.js
@@ -1,9 +1,26 @@
 var expect = require('chai').expect;
+var bundle = require('../dist/bundle.js');
 
-describe('API REST', function() {
-    describe('get coupon', function() {
-        it('a', function() {
-            expect(2).to.equal(2);
+new DatabaseHandler(process.env.DBPATH);
+
+describe('API REST', function () {
+    beforeEach(async () => {
+        var db = new bundle.DatabaseHandler(process.env.DBPATH);
+        db.dbclient.run('insert into promotion values(0,"TEST1"),(1,"TEST2"),(2,"TEST3")');
+        db.dbclient.run('insert into qrcode values(0,1),(1,2),(2,0)');
+    });
+    afterEach(() => {
+        db.close();
+    });
+
+    describe('get coupon', function () {
+        it('a', function () {
+            db.getCodePromoByQrCodeId('1', (row) => {
+                row.code.should.equal("TEST3");
+                done();
+            }, (error) => {
+                done();
+            });
         });
     });
 });
