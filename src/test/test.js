8bc985a6658106ec372b4d2a218e042e5244edb5
stephannicolas96 <stephannicolas96@hotmail.com>
2020-01-14 08:52:32 +0100

    DB + CI

---
+++ src/test/test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+describe('API REST', function() {
+    describe('get coupon', function() {
+        it('a', function() {
+            expect(2).to.equal(2);
+        });
+    });
+});
