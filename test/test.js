5223b4ee58adf53864eab8e25f702c0e097fc4fa
AlexLecq <alex.lecq6@gmail.com>
2020-01-17 17:07:30 +0100

    Add url in README - correct test 2

--- test/test.js
+++ test/test.js
@@ -4,14 +4,14 @@ var db = new CodePromoController(':memory:');
 
 describe('API REST', function () {
     before(async () => {
-        await db.dbclient.open(':memory:');
-        await db.dbclient.run('create table promotion(id int primary key,code text);');
-        await db.dbclient.run('create table qrcode(id int primary key,promotionId int,constraint fk_promotion_qrcode foreign key (promotionId) references promotion(id));');
-        await db.dbclient.run('insert into promotion values(0,"TEST1"),(1,"TEST2"),(2,"TEST3")');
-        await db.dbclient.run('insert into qrcode values(0,1),(1,2),(2,0)');
+        await db.sqliteHandler.open(':memory:');
+        await db.sqliteHandler.run('create table promotion(id int primary key,code text);');
+        await db.sqliteHandler.run('create table qrcode(id int primary key,promotionId int,constraint fk_promotion_qrcode foreign key (promotionId) references promotion(id));');
+        await db.sqliteHandler.run('insert into promotion values(0,"TEST1"),(1,"TEST2"),(2,"TEST3")');
+        await db.sqliteHandler.run('insert into qrcode values(0,1),(1,2),(2,0)');
     });
     after(async () => {
-        await db.dbclient.close();
+        await db.sqliteHandler.close();
     });
 
     describe('get coupon', () => {
@@ -20,7 +20,7 @@ describe('API REST', function () {
                                           FROM qrcode 
                                           INNER JOIN promotion ON qrcode.promotionId = promotion.id
                                           WHERE qrcode.id = ?`;
-            const result = await db.dbclient.get(queryToGetCodePromo, 1);
+            const result = await db.sqliteHandler.get(queryToGetCodePromo, 1);
             assert.equal(result.code, "TEST3");
         });
         it('when id is empty string result should be undefined', async () => {
@@ -28,7 +28,7 @@ describe('API REST', function () {
                                           FROM qrcode 
                                           INNER JOIN promotion ON qrcode.promotionId = promotion.id
                                           WHERE qrcode.id = ?`;
-            const result = await db.dbclient.get(queryToGetCodePromo, '');
+            const result = await db.sqliteHandler.get(queryToGetCodePromo, '');
             assert.equal(result, undefined);
         });
         it('when id is not in database result should be undefined', async () => {
@@ -36,7 +36,7 @@ describe('API REST', function () {
                                           FROM qrcode 
                                           INNER JOIN promotion ON qrcode.promotionId = promotion.id
                                           WHERE qrcode.id = ?`;
-            const result = await db.dbclient.get(queryToGetCodePromo, '12');
+            const result = await db.sqliteHandler.get(queryToGetCodePromo, '12');
             assert.equal(result, undefined);
         });
         it('when id is null result should be undefined', async () => {
@@ -44,7 +44,7 @@ describe('API REST', function () {
                                           FROM qrcode 
                                           INNER JOIN promotion ON qrcode.promotionId = promotion.id
                                           WHERE qrcode.id = ?`;
-            const result = await db.dbclient.get(queryToGetCodePromo, null);
+            const result = await db.sqliteHandler.get(queryToGetCodePromo, null);
             assert.equal(result, undefined);
         });
     });
