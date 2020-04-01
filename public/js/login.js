927133d4919ddbbe5fae2c55ae885adfdb50e1ef
AlexLecq <alex.lecq6@gmail.com>
2020-04-01 08:39:14 +0200

    finalisation de l'espace de gestion des codes promos

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,11 @@
-const errorDiv = document.querySelector('#error');
+const alert = document.querySelector('#error');
 window.sessionStorage.clear();
+
 const displayError = (errormessage) => {
-    errorDiv.innerHTML = `<div class="alert alert-danger alert-dismissible" id="error" role="alert">
-                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                    &times;
-                                </button>${errormessage}
-                            </div>`
+    console.log(errormessage);
+    alert.innerHTML = `<div class="alert alert-danger alert-dismissible" id="error" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="close">&times;</button>${errormessage}
+    </div>`;
 };
 const loginInput = document.querySelector('#login');
 const passwordInput = document.querySelector('#password');
@@ -15,17 +15,19 @@ loginForm.onsubmit = (event) => {
     fetch('/login', {
         method: 'POST',
         mode: 'cors',
+        headers:{
+            'Content-Type':'application/json'
+        },
         body: JSON.stringify({
             login: loginInput.value,
             password: passwordInput.value
         }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
+
     }).then(async (response) => {
         const json = await response.json();
+        alert.innerHTML = '';
         window.sessionStorage.setItem('token', json.token);
-        window.location.replace('/promocodes');
+        window.location.replace('/promocode.html');
     }).catch((error) => {
         displayError('Login/ Mot de passe incorrect');
     });
